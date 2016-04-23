dfb5d7d5ec6c326e5afdfea1bc9016b5196e3436
vivekvpandya <vivekvpandya@gmail.com>
2016-04-24 05:08:16 +0530

    Open MP parallelism applied, require to debug

--- include/userCode.h
+++ include/userCode.h
@@ -4,11 +4,16 @@
 #include <stdio.h>
 #include "dataStructures.h"
 
+// Following definitions will be provided by the framework -------------
 extern long globalBound;
 extern vector <long> currentSol;
 extern long **inputArray;
 extern int limit;
 extern set <Node *> liveNodes;
+
+extern void insertLiveNode(Node *solution);
+extern void updateBestSolution(Node *solution);
+// Framework definitions ends here -------------------------------------
 
 void initialize(void * root);
 void branch(void * SubPro);
